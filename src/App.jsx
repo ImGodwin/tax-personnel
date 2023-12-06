b9de58c9ef039c431e4cf2982a5bf28e3ea89da6
Godwin <godwinmail6@gmail.com>
2023-12-06 22:16:49 +0100

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TopBar from "./Components/TopBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <TopBar />;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <TopBar />
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

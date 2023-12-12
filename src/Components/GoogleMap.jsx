6490653b4ff16c362ca2d6ae650a73ac7dd7f1aa
Godwin <godwinmail6@gmail.com>
2023-12-12 15:44:52 +0100

    added redux folder, created the store

--- src/Components/GoogleMap.jsx
+++ src/Components/GoogleMap.jsx
@@ -4,7 +4,7 @@ const GoogleMap = () => {
   const position = { lat: 41.9028, lng: 12.4964 };
 
   return (
-    <APIProvider apiKey={""}>
+    <APIProvider apiKey={"AIzaSyAagwjxacnMsp2PQFdb2hLzuU1v4en9cWM"}>
       <Map center={position} zoom={10}>
         <Marker position={position} />
       </Map>
